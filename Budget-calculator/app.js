e312898116374a1eeabd98e6e2e80673d775a93e
Antonijo <antonijodev@gmail.com>
2019-06-26 14:18:40 +0100

    Creating a initialization function

--- Budget-calculator/app.js
+++ Budget-calculator/app.js
@@ -27,25 +27,34 @@ var UIController = (function() {
 
 // Global controller
 var controller = (function(budgetCtrl, UICtrl) {
-  var DOM = UICtrl.getDOMstrings();
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    document
+      .querySelector(DOM.inputButton)
+      .addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
 
   var ctrlAddItem = function() {
     // 1. Get the filed input data
     var input = UICtrl.getInput();
-    console.log(input);
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
   };
 
-  document
-    .querySelector(DOM.inputButton)
-    .addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function(event) {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  return {
+    init: function() {
+      setupEventListeners();
     }
-  });
+  };
 })(budgetController, UIController);
+
+controller.init();
