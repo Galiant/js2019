b28adab8e725479cba834c773234e83e0219a49e
Antonijo <antonijodev@gmail.com>
2019-06-26 14:07:55 +0100

    Reading input data

--- Budget-calculator/app.js
+++ Budget-calculator/app.js
@@ -3,22 +3,45 @@ var budgetController = (function() {})();
 
 // UI controller
 var UIController = (function() {
-  // Some code
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputButton: '.add__btn'
+  };
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 })();
 
 // Global controller
 var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMstrings();
+
   var ctrlAddItem = function() {
     // 1. Get the filed input data
+    var input = UICtrl.getInput();
+    console.log(input);
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
-
-    console.log('It works.');
   };
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document
+    .querySelector(DOM.inputButton)
+    .addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event) {
     if (event.keyCode === 13 || event.which === 13) {
