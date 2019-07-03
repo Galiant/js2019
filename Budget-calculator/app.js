2f904ec10ecd19b81079f1216d06f90c0925a5ef
Antonijo <antonijodev@gmail.com>
2019-07-03 14:37:56 +0100

    Updating the budget UI controller

--- Budget-calculator/app.js
+++ Budget-calculator/app.js
@@ -78,7 +78,7 @@ var budgetController = (function() {
         budget: data.budget,
         totalInc: data.total.inc,
         totalExp: data.total.exp,
-        percenrage: data.percentage
+        percentage: data.percentage
       };
     }
   };
@@ -92,7 +92,11 @@ var UIController = (function() {
     inputValue: '.add__value',
     inputButton: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   };
 
   return {
@@ -142,6 +146,20 @@ var UIController = (function() {
       fieldsArr[0].focus();
     },
 
+    displayBudget: function(obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent =
+        obj.totalExp;
+
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          obj.percentage + '%';
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '----';
+      }
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -172,7 +190,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     var budget = budgetCtrl.getBudget();
 
     // 3. Display the budget on the UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
   var ctrlAddItem = function() {
@@ -198,6 +216,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   return {
     init: function() {
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   };
