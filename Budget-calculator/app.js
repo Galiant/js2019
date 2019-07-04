e1554014bd6af2ca1919afaa1f50a4b908ecc886
Antonijo <antonijodev@gmail.com>
2019-07-04 16:53:02 +0100

    Finishing touches - improving UX

--- Budget-calculator/app.js
+++ Budget-calculator/app.js
@@ -163,6 +163,12 @@ var UIController = (function() {
     return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
   };
 
+  var nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
+
   return {
     getInput: function() {
       return {
@@ -243,12 +249,6 @@ var UIController = (function() {
         DOMstrings.expensesPercentageLabel
       );
 
-      var nodeListForEach = function(list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      };
-
       nodeListForEach(fields, function(current, index) {
         if (percentages[index] > 0) {
           current.textContent = percentages[index] + '%';
@@ -285,6 +285,21 @@ var UIController = (function() {
         months[month] + ' ' + year;
     },
 
+    changedType: function() {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType +
+          ',' +
+          DOMstrings.inputDescription +
+          ',' +
+          DOMstrings.inputValue
+      );
+
+      nodeListForEach(fields, function(current) {
+        current.classList.toggle('red-focus');
+      });
+      document.querySelector(DOMstrings.inputButton).classList.toggle('red');
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -308,6 +323,10 @@ var controller = (function(budgetCtrl, UICtrl) {
     document
       .querySelector(DOM.container)
       .addEventListener('click', ctrlDeleteItem);
+
+    document
+      .querySelector(DOM.inputType)
+      .addEventListener('change', UICtrl.changedType);
   };
 
   var updateBudget = function() {
