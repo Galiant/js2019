e1062a224da28cc6d709272b619acc1dd7b93ec5
Antonijo <antonijodev@gmail.com>
2019-08-15 15:08:21 +0100

    Building the search model

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,15 +1,6 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-  const key = 'f08150c5e65790c8b43918c25f0b32b8';
-  try {
-    const res = await axios(
-      `https://www.food2fork.com/api/search?key=${key}&q=${query}`
-    );
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error);
-  }
-}
-getResults('pizza');
+const search = new Search('pizza');
+console.log(search);
+
+search.getResults();
